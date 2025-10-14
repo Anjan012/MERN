7c53103f75a277b2bbe4eb58714dd4c1a037a1f5
Anjan012 <anjankhadka199@gmail.com>
2025-10-14 20:21:15 +0545

    setup for frontend

---
+++ client/src/App.jsx
@@ -0,0 +1,13 @@
+const App = () => {
+  return <>
+    <div>
+      <h1>Resume</h1> <hr />
+      <h3>Name: Anjan khadka</h3>
+      <h3>Age: 21</h3>
+      <h3>Full Stack Developer (MERN) </h3>
+
+    </div>
+  </>
+}
+
+export default App;
