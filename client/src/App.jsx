191e579f9e8b534c8e308b8f592529ae96618adc
Anjan012 <anjankhadka199@gmail.com>
2025-10-15 21:24:23 +0545

    learn browserRouter and use it to implement it in project

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,27 @@
+import {BrowserRouter, Routes, Route} from "react-router-dom";
+import { Home } from "./pages/Home";
+import { About } from "./pages/About";
+import { Contact } from "./pages/Contact";
+import { Service }  from "./pages/Service";
+import { Register } from "./pages/Register";
+import { Login } from "./pages/Login";
+
+
 const App = () => {
   return <>
-    <div>
-      <h1>Resume</h1> <hr />
-      <h3>Name: Anjan khadka</h3>
-      <h3>Age: 21</h3>
-      <h3>Full Stack Developer (MERN) </h3>
+   <BrowserRouter>
+    <Routes>
+      <Route  path="/" element={<Home />}/>
+      <Route  path="/about" element={<About />}/>
+      <Route  path="/contact" element={<Contact />}/>
+      <Route  path="/service" element={<Service />}/>
+      <Route  path="/register" element={<Register />}/>
+      <Route  path="/login" element={<Login />}/>
 
-    </div>
+
+
+    </Routes>
+   </BrowserRouter>
   </>
 }
 
